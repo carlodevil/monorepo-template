ff0b6865669cf4d59a2ac1cb29af8a60da9aff5a
Carlo de Villiers <carlodevil1@gmail.com>
2021-04-28 11:28:36 +0200

    Installed/config main app and dev tools

---
+++ packages/app-main/.yalc/button-generic/src/GenericBtn.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Slot start here: <slot/> :Slot end here
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ packages/app-main/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <br><btn>asd</btn>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+import GenericBtn from 'button-generic';
+
+export default {
+  name: 'Home',
+  components: {
+    btn: GenericBtn,
+  },
+};
+</script>
